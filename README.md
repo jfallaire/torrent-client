4e7b5e962eb7596cc5bb7508cbb5a25b847572a8
Jean-Francois Allaire <jeanfrancois.allaire@gmail.com>
2018-03-18 09:12:56 -0400

    update package.json - add preferGlobal option

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # torrent-client
+
+# References
+
+    * https://scotch.io/tutorials/build-an-interactive-command-line-application-with-nodejs
--- client.js
+++ client.js
@@ -8,31 +8,31 @@ const torrentSearch = new TorrentSearchApi();
 torrentSearch.enableProvider('1337x');
 
 program
-  .version('0.0.1')
-  .description('Torrent search client');
+    .version('0.0.1')
+    .description('Torrent search client');
 
 program
-  .command('search <query>')
-  .alias('s')
-  .description('search for torrents')
-  .action((query) => {
-	torrentSearch.search(query, 'Movies', 20)
-     	.then(torrents => {
-         	console.log(torrents);
-     	})
-     	.catch(err => {
-        	 console.log(err);
-     });
-  });
+    .command('search <query>')
+    .alias('s')
+    .description('search for torrents')
+    .action((query) => {
+        torrentSearch.search(query, 'Movies', 20)
+            .then(torrents => {
+                console.log(torrents);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
 
 program
-  .command('getProviderList')
-  .alias('l')
-  .description('List providers')
-  .action(() => {
-	//getProviderList();
-	// Get providers
-	console.log(torrentSearch.getProviders());
-  });
+    .command('getProviderList')
+    .alias('l')
+    .description('List providers')
+    .action(() => {
+        //getProviderList();
+        // Get providers
+        console.log(torrentSearch.getProviders());
+    });
 
 program.parse(process.argv)
