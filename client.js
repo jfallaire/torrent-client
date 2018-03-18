e9e5f7548f92d135877db9e81114c1df6e63f3d8
Jean-Francois Allaire <jeanfrancois.allaire@gmail.com>
2018-03-18 13:03:46 -0400

    update client - missing comma for options

--- client.js
+++ client.js
@@ -15,8 +15,8 @@ program
     .command('search [options] <query>')
     .alias('s')
     .description('search for torrents')
-    .option('-c --category <category>', 'Which search category to use')
-    .option('-l --limit <limit>', 'Maximum Number of returned results')
+    .option('-c, --category <category>', 'Which search category to use')
+    .option('-l, --limit <limit>', 'Maximum Number of returned results')
     .action((query, options) => {
         var category = options.category || "All";
         var limit = options.limit || '20';
